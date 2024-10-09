457df5bc7a2d4707ede19c8622f96fd839820e7d
Jorgen Skogmo <js@dearstudio.dk>
2024-10-09 11:43:42 +0200

    html.customData and react.d.ts

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,38 @@ import * as React from "react";
 import "atom-lit";
 
 function MyButton({ title }: { title: string }) {
-	return <atom-button variant="constructive"></atom-button>;
-	// return <button>{title}</button>;
+	// even HTML Custom Elements can be used as React components and are self-closing
+	const onChange = (e: Event) => {
+		console.log("Button clicked");
+	};
+
+	return (
+		<atom-button
+			variant="outline"
+			label="my button 2"
+			onClick={onChange}
+			onKeyUp={onChange}
+		/>
+	);
 }
 
 export function App() {
 	return (
 		<div>
-			<h1>Welcome to my app</h1>
+			<h1>Rendered as a rect component:</h1>
 			<MyButton title="I'm a button" />
+
+			<atom-range onChange={(e: Event) => console.log("Range changed")} />
+
+			<atom-icon name="align-center" />
 		</div>
 	);
 }
 
 const container = document.getElementById("app");
-const root = createRoot(container!);
-root.render(<App />);
+if (container) {
+	const root = createRoot(container);
+	root.render(<App />);
+} else {
+	console.error("Failed to find the app container");
+}
